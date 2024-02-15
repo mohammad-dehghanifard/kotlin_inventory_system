e66d4bf1f3aecee27f3ead40ac2ef4e656a809b8
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-15 05:42:30 -0800

    impl FileUtility class

---
+++ src/main/kotlin/utils/FileUtility.kt
@@ -0,0 +1,21 @@
+package utils
+import java.io.File
+
+class FileUtility {
+    companion object {
+        // add text to file
+        suspend fun appendToFile(filename : String,text : String) = File(filename).appendText("$text\r\n")
+        // read text
+        suspend fun readFile(filename:String) {
+            if(File(filename).exists()) {
+                File(filename).forEachLine {  }
+            } else {
+                println("فایل مورد نظر وجود ندارد، لطفا یک فایل جدید ایجاد کنید.")
+            }
+        }
+        // write new text to file
+        suspend fun write(filename : String,text : String) {
+            File(filename).writeText("$text\r\n")
+        }
+    }
+}
