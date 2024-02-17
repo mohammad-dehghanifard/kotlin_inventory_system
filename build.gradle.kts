ab7a7989a34ec9f10771ff3413adc2e4cffd3687
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-17 08:35:45 -0800

    impl toJson and from json function for Product data class

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/entities/Product.kt
+++ src/main/kotlin/entities/Product.kt
@@ -1,9 +1,23 @@
 package entities
 
+import com.google.gson.Gson
+
 data class Product(
     val name : String,
     val description : String,
     val sku : String,
     var price : Long,
     var quantity : Int
-)
+) {
+    fun toJson() : String {
+        val gson = Gson()
+        return gson.toJson(this)
+    }
+
+    companion object {
+        fun fromJson(json: String): Product {
+            val gson = Gson()
+            return gson.fromJson(json, Product::class.java)
+        }
+    }
+}
